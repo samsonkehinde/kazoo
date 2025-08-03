272a8c12e1ce615d980e74ae07647c0f10f42b3c
Samson Kehinde <41105905+samsonkehinde@users.noreply.github.com>
2025-08-03 19:47:47 +0100

    update2 to try_catch_clause

--- core/kazoo/src/kz_util.erl
+++ core/kazoo/src/kz_util.erl
@@ -7,7 +7,7 @@
 %%%-----------------------------------------------------------------------------
 -module(kz_util).
 
--export([log_stacktrace/1, log_stacktrace/2, log_stacktrace/3
+-export([log_stacktrace/1, log_stacktrace/3
         ,format_account_id/1, format_account_id/2, format_account_id/3
         ,format_account_mod_id/1, format_account_mod_id/2, format_account_mod_id/3
         ,format_account_db/1
@@ -87,10 +87,10 @@
 log_stacktrace(ST) ->
     log_stacktrace(ST, "", []).
 
--spec log_stacktrace(string(), list()) -> ok.
-log_stacktrace(Fmt, Args) ->
-    ST = erlang:get_stacktrace(),
-    log_stacktrace(ST, Fmt, Args).
+#-spec log_stacktrace(string(), list()) -> ok.
+#log_stacktrace(Fmt, Args) ->
+#    ST = erlang:get_stacktrace(),
+#    log_stacktrace(ST, Fmt, Args).
 
 -spec log_stacktrace(list(), string(), list()) -> ok.
 log_stacktrace(ST, Fmt, Args) ->
