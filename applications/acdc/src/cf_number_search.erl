c21c0934bc98d84d2a1cf129d4ad4ed74432aa3c
Samson Kehinde <kehindesamson@gmail.com>
2025-09-25 03:09:10 +0100

    updated, callflow hijack. Did not work

--- applications/acdc/src/cf_number_search.erl
+++ applications/acdc/src/cf_number_search.erl
@@ -5,41 +5,42 @@
 -include_lib("callflow/src/callflow.hrl").
 
 -spec handle(kz_json:object(), kapps_call:call()) -> 'ok'.
-handle(Data, Call) ->
-    Number = kz_json:get_ne_binary_value(<<"number">>, Data),
-    CalledNumber = kapps_call:request_user(Call),
+handle(_Data, Call) ->
+    %%Number = kz_json:get_ne_binary_value(<<"queue_number">>, Data),
+    %%CalledNumber = kapps_call:kvs_fetch('queue_number', Call),
+    Number = kapps_call:kvs_fetch('cf_capture_group', Call),
 
-    lager:info("Request User: ~s", [CalledNumber]),
-    lager:info("Called Number: ~s", [Number]),
+    %%lager:info("Queue Number: ~s", [Number]),
+    %%lager:info("Queue Number: ~s", [CalledNumber]),
+    lager:info("Queue Number: ~s", [Number]),
 
     Accounts = kapps_util:get_all_accounts(),
 
-    Results = [find_in_account(Account, Number) || Account <- Accounts],
+    Results = lists:flatten([find_in_account(Account, Number) || Account <- Accounts]),
+    lager:info("Found callflows: ~p", [Results]),
 
     case length(Results) > 0 of
         true ->
             %% send the call to the first callflow found.
-            [CF|_] = Results,
-            lager:info("found callflow ~s for number ~s", [CF, Number]),
-            cf_exe:flow(CF, Call);
+            [{Id, Cf}|_] = Results,
+            lager:info("found callflow ~s for number ~s", [Id, Number]),
+            cf_exe:continue_with_flow(Cf, Call);
         false ->
             lager:info("no callflow found for number ~s", [Number]),
             cf_exe:continue(Call)
     end.
 
 find_in_account(AccountId, Number) ->
-    Db = kz_util:format_account_db(AccountId, <<"callflows">>),
-    case kz_datamgr:get_results(Db, <<"_design/callflows/_view/listing">>, []) of
+    lager:info("Search for callflows number ~p in account ~p", [Number, AccountId]),
+    Db = kz_util:format_account_db(AccountId),
+    case kz_datamgr:get_results(Db, <<"callflows/listing_by_number">>,[{key, Number}, include_docs]) of
         {ok, Callflows} ->
             lists:foldl(
-              fun(#{doc := Doc}, Acc) ->
-                  Numbers = kz_json:get_value([<<"numbers">>], Doc, []),
-                  case lists:member(Number, Numbers) of
-                      true -> 
-                        CF = kz_json:get_ne_binary_value([<<"flow">>, <<"module">>], Doc),
-                        [ CF | Acc];
-                      false -> Acc
-                  end
+              fun(Doc, Acc) ->
+                    CF = kz_json:get_value([<<"doc">>, <<"flow">>], Doc),
+                    ID = kz_json:get_value([<<"doc">>, <<"_id">>], Doc),
+                    lager:debug("Callflow : ~p", [CF]),
+                    [{ID, CF} | Acc]
               end, [], Callflows);
         {error, _} ->
             []
